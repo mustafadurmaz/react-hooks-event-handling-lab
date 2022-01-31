6fde571dc8ed7a38924ae8752f036ca041a3cc6a
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-01-31 12:32:51 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+const EyesOnMe = () => {
+    function handleFocus(event) {
+        console.log("Good!");
+      }
+    
+      function handleBlur(event) {
+        console.log("Hey! Eyes on me!");
+      }
+    
+      return (
+        <div>
+          <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+          
+        </div>
+        
+      );
+};
+
+export default EyesOnMe;
+
