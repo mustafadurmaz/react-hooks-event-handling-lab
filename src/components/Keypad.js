6c2363a601fec673dbe0ba0a01d7f4cd784489a7
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-01-31 12:32:18 +0300

    commit tree f14546681f069fed60c6073b98874fabb92e02e6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
+
+const Keypad = () => {
+  function handleChange(event) {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        placeholder="Enter search term..."
+      />
+    </div>
+  );
+};
+
+export default Keypad;
